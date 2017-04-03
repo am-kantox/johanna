ee79264a33b47e56f2731a7738c4371b6766b943
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2017-04-03 10:01:28 +0200

    Task almost done.

--- apps/johanna/lib/mix/tasks/unmbrella_hex.ex
+++ apps/johanna/lib/mix/tasks/unmbrella_hex.ex
@@ -10,11 +10,11 @@ defmodule Mix.Tasks.UnmbrellaHex do
   def run(argv) do
     File.rm_rf!(@build_dir)
     File.mkdir!(@build_dir)
-    File.cp_r("apps/johanna/", @build_dir)
+    File.cp_r!("apps/johanna/", @build_dir)
 
     erlcron = Path.join(@build_dir, "src")
     File.mkdir!(erlcron)
-    File.cp_r("apps/erlcron/src", erlcron)
+    File.cp_r!("apps/erlcron/src", erlcron)
 
     mix_file = Path.join(@build_dir, "mix.exs")
     mix_content = mix_file
@@ -23,7 +23,19 @@ defmodule Mix.Tasks.UnmbrellaHex do
                       ~r|\[extra_applications: \[:logger, :erlcron\]\]|,
                       "[extra_applications: [:logger], mod: {:ecrn_app, []}]"
                   )
+                  |> String.replace(
+                      ~r|\s*\w+\:\s*\"\.\.\/\.\.\/.*?\",\s*|,
+                      ""
+                  )
     File.write!(mix_file, mix_content)
-    File.cd!(@build_dir, fn -> Mix.Tasks.Hex.Publish.run end)
+
+    File.cd!(@build_dir, fn ->
+      spawn(fn ->
+        Mix.Tasks.Deps.Get.run(argv)
+        Mix.Tasks.Deps.Compile.run(argv)
+        Mix.Tasks.Compile.run(argv)
+        Mix.Tasks.Hex.Publish.run(argv)
+      end)
+    end)
   end
 end
