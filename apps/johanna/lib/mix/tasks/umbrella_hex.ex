9e7c52086d430cb9134eac5a318985cb1cf46b43
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2017-04-06 13:50:31 +0200

    Still trying erlcron app.

--- apps/johanna/lib/mix/tasks/umbrella_hex.ex
+++ apps/johanna/lib/mix/tasks/umbrella_hex.ex
@@ -15,8 +15,9 @@ defmodule Mix.Tasks.UmbrellaHex do
     erlcron = Path.join(@build_dir, "src")
     File.mkdir!(erlcron)
     File.cp_r!("apps/erlcron/src", erlcron)
-    # File.cd!("apps/erlcron", fn -> System.cmd("make", []) end)
-    # File.cp!("apps/erlcron/ebin/erlcron.app", @build_dir <> "/erlcron.app")
+
+    File.cd!("apps/erlcron", fn -> System.cmd("make", []) end)
+    File.cp!("apps/erlcron/ebin/erlcron.app", @build_dir <> "/erlcron.app")
 
     mix_file = Path.join(@build_dir, "mix.exs")
     mix_content = mix_file
